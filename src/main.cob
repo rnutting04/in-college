0b06dd1004a951b112bf69592fe2c62dbf18339a
mmosoriov <mmosoriov@hotmail.com>
2025-09-06 16:03:24 +0000

    created the layout of the program, loads user data and displays main menu

--- src/main.cob
+++ src/main.cob
@@ -8,6 +8,10 @@
                    SELECT OutputFile ASSIGN TO "InCollege-Output.txt"
                        ORGANIZATION IS LINE SEQUENTIAL
                        ACCESS IS SEQUENTIAL.
+                   *> Sequential file to store Users data
+                   SELECT UsersFile ASSIGN TO "InCollege-Users.txt"
+                       ORGANIZATION IS LINE SEQUENTIAL
+                       ACCESS IS SEQUENTIAL.
       DATA DIVISION.
            FILE SECTION.
                FD  InputFile.
@@ -16,29 +20,77 @@
                FD  OutputFile.
                01  OutputRecord PIC X(100).
 
+               FD UsersFile.
+               01 UserRecord.
+                   *> Sub-records start with 05
+                   05 UR-Username PIC X(20).
+                   05 Space-In-Between PIC X VALUE SPACE.
+                   05 UR-Password PIC X(12).
+
            WORKING-STORAGE SECTION.
-               *> boolean initialized to N which means we haven reached the end of file
-               01  EndOfFile   PIC X VALUE "N".
+           *> Flag variable initialized to N (havent reached the end of file)
+           01 WS-EOF-Flag PIC X VALUE "N".
+               88 EOF VALUE "Y".
+
+            01 WS-Number-Users PIC 9 VALUE 0.
+
+           01 WS-User-Table.
+               05 WS-User OCCURS 5 TIMES.
+                   10 WS-Username PIC X(20).
+                   10 WS-Password PIC X(12).
+
+           01 WS-Line             PIC X(100).
 
       PROCEDURE DIVISION.
-           OPEN INPUT InputFile
-           OPEN OUTPUT OutputFile
+           MAIN.
+               OPEN INPUT InputFile
+               OPEN OUTPUT OutputFile
+
+               PERFORM LOAD-USERS
+
+               PERFORM DISPLAY-MAIN-MENU
+
+               *>PERFORM PROCESS-INITIAL-CHOICE
+
+               *>IF Logged-In
+                   *>PERFORM POST-LOGIN-MENU
+               *>END-IF
+
+               *>PERFORM SAVE-USERS
+
+               CLOSE InputFile
+               CLOSE OutputFile
+               STOP RUN.
+
+           LOAD-USERS.
+               OPEN INPUT UsersFile
+               *> Initialize count to 0
+               MOVE 0 TO WS-Number-Users
+               PERFORM UNTIL WS-Number-Users = 5 OR EOF
+                   READ UsersFile INTO UserRecord
+                       AT END SET EOF TO TRUE
+                       NOT AT END
+                           ADD 1 TO WS-Number-Users
+                           MOVE UR-Username TO WS-Username(WS-Number-Users)
+                           MOVE UR-Password TO WS-Password(WS-Number-Users)
+                   END-READ
+               END-PERFORM
+               CLOSE UsersFile.
 
 
-           DISPLAY "Iowa InCollege project"
 
-           PERFORM UNTIL EndOfFile = "Y"
-               READ InputFile
-                   AT END
-                       MOVE "Y" TO EndOfFile
-                   NOT AT END
-                       DISPLAY InputRecord
-               END-READ
-           END-PERFORM
-
-
-           CLOSE InputFile
-           CLOSE OutputFile
+           DISPLAY-MAIN-MENU.
+               MOVE "Welcome to InCollege!" TO WS-Line
+               PERFORM OUTPUT-LINE
+               MOVE "Log In" TO WS-Line
+               PERFORM OUTPUT-LINE
+               MOVE "Create New Account" TO WS-Line
+               PERFORM OUTPUT-LINE
+               MOVE "Enter your choice:" TO WS-Line
+               PERFORM OUTPUT-LINE.
 
 
-           STOP RUN.
+           OUTPUT-LINE.
+               MOVE WS-Line TO OutputRecord
+               DISPLAY WS-Line
+               WRITE OutputRecord.
