f882664a9251b4d1634e439491c6e5fe346236be
mmosoriov <mmosoriov@hotmail.com>
2025-09-06 03:13:48 +0000

    added input/output files

--- src/main.cob
+++ src/main.cob
@@ -1,8 +1,44 @@
-*> This is free-form
-IDENTIFICATION DIVISION.
-PROGRAM-ID. MAIN.
-ENVIRONMENT DIVISION.
-DATA DIVISION.
-PROCEDURE DIVISION.
-    DISPLAY "Iowa InCollege project"
-    STOP RUN.
+      IDENTIFICATION DIVISION.
+           PROGRAM-ID. InCollege.
+      ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION.
+               FILE-CONTROL.
+                   SELECT InputFile ASSIGN TO "InCollege-Input.txt"
+                       ORGANIZATION IS LINE SEQUENTIAL.
+                   SELECT OutputFile ASSIGN TO "InCollege-Output.txt"
+                       ORGANIZATION IS LINE SEQUENTIAL
+                       ACCESS IS SEQUENTIAL.
+      DATA DIVISION.
+           FILE SECTION.
+               FD  InputFile.
+               *>File description
+               01  InputRecord PIC X(100).
+               FD  OutputFile.
+               01  OutputRecord PIC X(100).
+
+           WORKING-STORAGE SECTION.
+               *> boolean initialized to N which means we haven reached the end of file
+               01  EndOfFile   PIC X VALUE "N".
+
+      PROCEDURE DIVISION.
+           OPEN INPUT InputFile
+           OPEN OUTPUT OutputFile
+
+
+           DISPLAY "Iowa InCollege project"
+
+           PERFORM UNTIL EndOfFile = "Y"
+               READ InputFile
+                   AT END
+                       MOVE "Y" TO EndOfFile
+                   NOT AT END
+                       DISPLAY InputRecord
+               END-READ
+           END-PERFORM
+
+
+           CLOSE InputFile
+           CLOSE OutputFile
+
+
+           STOP RUN.
